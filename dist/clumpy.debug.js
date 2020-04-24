71ce63d5cc0b9f8378a9292a2b532c3680e27ba1
Thomas Peri <tjperi@gmail.com>
2020-04-24 00:43:50 -0700

    dev version bump in dist

--- dist/clumpy.debug.js
+++ dist/clumpy.debug.js
@@ -1,6 +1,6 @@
 (function(P,U,L){L(U,P);}(this,function(){'use strict';
 /*!
- * Clumpy v1.1.0
+ * Clumpy v1.1.1-dev
  * https://github.com/thomasperi/clumpyjs#readme
  * Thomas Peri <hello@thomasperi.net>
  * MIT License
